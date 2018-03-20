e1caba994a383daa8f20d7efef04c2aa9718783f
Ricardo Simões <xmcorporation@gmail.com>
2018-03-20 22:33:08 +0000

    Update 100-display-manager-and-desktop-v1.sh

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -20,7 +20,7 @@ echo "#################################################"
 
 sudo pacman -Syyu
 sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm --needed
-sudo pacman -S xfce4 xfce4-goodies --noconfirm --needed
+sudo pacman -S xfce4 xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin --noconfirm --needed
 sudo systemctl enable lightdm.service -f
 sudo systemctl set-default graphical.target
 
@@ -28,4 +28,4 @@ sudo systemctl set-default graphical.target
 echo "Remove anything you do not like from the installed applications"
 
 #sudo pacman -R ...
-sudo pacman -R xfce4-artwork --noconfirm
+#sudo pacman -R xfce4-artwork --noconfirm
